545445a8b5e4a066241a0427074695a4e40c0d80
Sahil Saini <sahilsaini137@gmail.com>
2023-02-10 22:03:33 +0530

    set up ts paths and mongoose

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,18 @@
-import express, { Application, Request, Response } from "express";
+import express, { Application, Request, Response } from 'express';
+import dbConnection from '@config/mongoose';
+import dotenv from 'dotenv';
+import morgan from 'morgan';
+dotenv.config();
 
 const app: Application = express();
 
-const PORT = 8000;
-
-app.get("/", (req: Request, res: Response) => {
-  res.json({ message: "data recieved" });
+dbConnection();
+app.use(morgan('dev'));
+app.get('/', (req: Request, res: Response) => {
+  res.json({ message: 'data recieved' });
 });
 
+const PORT = 8000;
 app.listen(PORT, () => {
-  console.log(`Server Running on PORT :: ${PORT}`);
+  console.log(`🚀 Server Running on PORT :: ${PORT} 🚀`);
 });
